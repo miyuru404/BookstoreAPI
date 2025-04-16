8617046cf1b1f943e1994f8c4e80c42e77715e85
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-17 01:35:27 +0530

    add-updated the project structure and add classes

--- BookstoreAPI/src/model/Book.java
+++ BookstoreAPI/src/model/Book.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Book {
+    private String title;
+    private String author;
+    private int ISBN;
+    private String publicationYear;
+    private int price;
+    private int stockQuantity;
+
+    public Book() {}
+
+    public Book(String title, String author, int ISBN, String publicationYear, int price, int stockQuantity) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+    public String getTitle() {return title;}
+    public String getAuthor() {return author;}
+    public int getISBN() {return ISBN;}
+    public String getPublicationYear() {return publicationYear;}
+    public int getPrice() {return price;}
+    public int getStockQuantity() {return stockQuantity;}
+
+    public void setTitle(String title) {this.title = title;}
+    public void setAuthor(String author) {this.author = author;}
+    public void setISBN(int ISBN) {this.ISBN = ISBN;}
+    public void setPublicationYear(String publicationYear) {this.publicationYear = publicationYear;}
+    public void setPrice(int price) {this.price = price;}
+    public void setStockQuantity(int stockQuantity) {this.stockQuantity = stockQuantity;}
+
+
+
+
+
 }
--- BookstoreAPI/src/model/Cart.java
+++ BookstoreAPI/src/model/Cart.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.util.List;
+
 public class Cart {
+    private Customer customer ;
+    private List<Book> items;
+
+    public Cart(Customer customer, List<Book> items) {
+        this.customer = customer;
+        this.items = items;
+    }
 }
