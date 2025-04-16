5c7c3a0b6c8e8910e5598fe56d233d39a1f162fb
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-17 02:21:01 +0530

    add-updated the project structure and add classes

--- BookstoreAPI/src/model/Cart.java
+++ BookstoreAPI/src/model/Cart.java
@@ -5,9 +5,25 @@ import java.util.List;
 public class Cart {
     private Customer customer ;
     private List<Book> items;
+    private int valueOfTheCart;
 
     public Cart(Customer customer, List<Book> items) {
         this.customer = customer;
         this.items = items;
     }
+    public Customer getCustomer() {return customer;}
+    public List<Book> getItems() {return items;}
+    public int getValueOfTheCart() { return valueOfTheCart; }
+
+    public void setValueOfTheCart(int valueOfTheCart) { this.valueOfTheCart = valueOfTheCart; }
+    public double calculateTotal() {
+        double total = 0.0;
+        for (Book book : items) {
+            total += book.getPrice();
+        }
+        return total;
+    }
+    public void clearCart(){items.clear();}
+
+
 }
