47acff145380bc05b21d0595c1df1f35ea787c4d
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-17 13:39:25 +0530

    feat: add BookDataStore class

--- BookstoreAPI/src/modelData/BookDataStore.java
+++ BookstoreAPI/src/modelData/BookDataStore.java
@@ -8,5 +8,15 @@ import java.util.List;
 public class BookDataStore {
     private static final List<Book> books = new ArrayList<Book>();
 
+    public static Book getBook(String ISBN) {
+        return null;
+    }
+
+    public static void addBook(Book book) {}
+
+    public static void removeBook(Book book) {}
+
+    public static void updateBook(Book book) {}
+
 
 }
