3c3b9587d29088b6d71c9b33e49e926d153af745
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:15:50 +0530

    feat:add AuthorNotFoundException class

--- csf-cw/src/main/java/com/demo/exception/AuthorNotFoundException.java
+++ csf-cw/src/main/java/com/demo/exception/AuthorNotFoundException.java
@@ -1,4 +1,7 @@
 package com.demo.exception;
 
-public class AuthorNotFoundException {
+public class AuthorNotFoundException extends RuntimeException {
+    public AuthorNotFoundException(String message) {
+        super(message);
+    }
 }
