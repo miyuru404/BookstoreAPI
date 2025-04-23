0dad9113e8d132e2d54966ff4a2bbb87e6bb3b6f
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:32:23 +0530

    update:complete ExceptionMapper class

--- csf-cw/src/main/java/com/demo/exception/exceptionMapper/InvalidInputExceptionMapper.java
+++ csf-cw/src/main/java/com/demo/exception/exceptionMapper/InvalidInputExceptionMapper.java
@@ -1,4 +1,16 @@
 package com.demo.exception.exceptionMapper;
 
-public class InvalidInputExceptionMapper {
+import com.demo.exception.InvalidInputException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class InvalidInputExceptionMapper implements ExceptionMapper<InvalidInputException> {
+    @Override
+    public Response toResponse(InvalidInputException ex) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity("{\"error\": \"" + ex.getMessage() + "\"}")
+                .build();
+    }
 }
