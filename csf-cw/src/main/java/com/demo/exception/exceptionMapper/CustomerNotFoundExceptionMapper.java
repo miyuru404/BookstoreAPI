09ca7c434e3561d1ab87b2443213d463a6f3517f
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:23:11 +0530

    feat:add CustomerNotFoundExceptionMapper class

---
+++ csf-cw/src/main/java/com/demo/exception/exceptionMapper/CustomerNotFoundExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.demo.exception.exceptionMapper;
+
+import com.demo.exception.CustomerNotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+
+public class CustomerNotFoundExceptionMapper implements ExceptionMapper<CustomerNotFoundException> {
+    @Override
+    public Response toResponse(CustomerNotFoundException ex) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("{\"error\": \"" + ex.getMessage() + "\"}")
+                .build();
+    }
+}
