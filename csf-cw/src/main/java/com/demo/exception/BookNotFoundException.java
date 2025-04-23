dfb16e073f6c4d2524831716defa5237caeee959
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:15:12 +0530

    feat:add BookNotFoundException class

--- csf-cw/src/main/java/com/demo/exception/BookNotFoundException.java
+++ csf-cw/src/main/java/com/demo/exception/BookNotFoundException.java
@@ -1,4 +1,7 @@
 package com.demo.exception;
 
-public class BookNotFoundException {
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
 }
