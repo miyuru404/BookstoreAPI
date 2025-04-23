df63bb03dc088d6ecf4c1ddd73d53fa1d0d03bab
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:39:30 +0530

    update:complete ExceptionMapper class

--- csf-cw/src/main/java/com/demo/resource/AuthorResource.java
+++ csf-cw/src/main/java/com/demo/resource/AuthorResource.java
@@ -1,13 +1,12 @@
 package com.demo.resource;
 
 import com.demo.dataModel.AuthorDataStore;
+import com.demo.exception.AuthorNotFoundException;
+import com.demo.exception.InvalidInputException;
 import com.demo.model.Author;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
-
-
 
 @Path("author")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,65 +17,49 @@ public class AuthorResource {
     @Path("/name/{name}-{surname}")
     public Response getAuthorByName(@PathParam("name") String name, @PathParam("surname") String surname) {
         if (name == null || name.isEmpty() || surname == null || surname.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Author full name is missing\"}").build();
+            throw new InvalidInputException("Author full name is missing");
         }
 
         if (!name.matches("^[A-Za-z ]+$") || !surname.matches("^[A-Za-z ]+$")) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid name. Name should contain only letters and spaces.\"}")
-                    .build();
+            throw new InvalidInputException("Invalid name. Name should contain only letters and spaces.");
         }
 
         Author author = AuthorDataStore.getAuthor(name, surname);
         if (author == null) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("{\"error\":\"Author not found\"}").build();
+            throw new AuthorNotFoundException("Author not found");
         }
 
         return Response.ok(author).build();
     }
 
-
     @POST
     @Path("/add")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response addAuthor(Author author) {
         if (author == null) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Author details are missing.\"}").build();
+            throw new InvalidInputException("Author details are missing.");
         }
 
         if (author.getName() == null || author.getName().trim().isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Name is required.\"}").build();
+            throw new InvalidInputException("Name is required.");
         }
 
         if (author.getSurname() == null || author.getSurname().trim().isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Surname is required.\"}").build();
+            throw new InvalidInputException("Surname is required.");
         }
 
         if (!author.getName().matches("^[A-Za-z ]+$") || !author.getSurname().matches("^[A-Za-z ]+$")) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid name. Name should contain only letters and spaces.\"}")
-                    .build();
+            throw new InvalidInputException("Invalid name. Name should contain only letters and spaces.");
         }
 
         if (author.getAge() < 1 || author.getAge() > 100) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid age. Age must be between 0 and 100.\"}").build();
-        }
-        if (AuthorDataStore.getAuthor(author.getName(),author.getSurname()) != null) {
-            return Response.status(Response.Status.CONFLICT)
-                    .entity("{\"error\":\"Author with this name is already registered.\"}")
-                    .build();
+            throw new InvalidInputException("Invalid age. Age must be between 0 and 100.");
         }
 
+        if (AuthorDataStore.getAuthor(author.getName(), author.getSurname()) != null) {
+            throw new InvalidInputException("Author with this name is already registered.");
+        }
 
         AuthorDataStore.addAuthor(author);
-
 
         return Response.status(Response.Status.CREATED)
                 .entity("{\"error\":\"Author successfully added.\"}")
@@ -85,25 +68,18 @@ public class AuthorResource {
 
     @DELETE
     @Path("/delete/{name}-{surname}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Response removeCustomer(@PathParam("name") String name, @PathParam("surname") String surname) {
-        if (name == null || name.trim().isEmpty() ) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Name is required.\"}")
-                    .build();
-        }
-        if ( surname == null || surname.trim().isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Surname is required.\"}")
-                    .build();
+        if (name == null || name.trim().isEmpty()) {
+            throw new InvalidInputException("Name is required.");
         }
 
+        if (surname == null || surname.trim().isEmpty()) {
+            throw new InvalidInputException("Surname is required.");
+        }
 
         Author author = AuthorDataStore.getAuthor(name, surname);
         if (author == null) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("{\"error\":\"Author not found.\"}")
-                    .build();
+            throw new AuthorNotFoundException("Author not found.");
         }
 
         AuthorDataStore.deleteAuthor(name, surname);
@@ -114,53 +90,38 @@ public class AuthorResource {
 
     @PUT
     @Path("/update/{name}-{surname}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response updateCustomer(@PathParam("name") String name, @PathParam("surname") String surname, Author updatedAuthor) {
         if (updatedAuthor == null) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Updated Author object is missing.\"}")
-                    .build();
+            throw new InvalidInputException("Updated Author object is missing.");
         }
 
         if (updatedAuthor.getName() == null || updatedAuthor.getName().trim().isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"New name is required\"}")
-                    .build();
+            throw new InvalidInputException("New name is required.");
         }
 
         if (!updatedAuthor.getName().matches("^[A-Za-z ]+$")) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid name. Only letters allowed.\"}")
-                    .build();
+            throw new InvalidInputException("Invalid name. Only letters allowed.");
         }
 
         if (updatedAuthor.getSurname() == null || updatedAuthor.getSurname().trim().isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"New surname is required.\"}")
-                    .build();
+            throw new InvalidInputException("New surname is required.");
         }
 
-        if (!updatedAuthor.getSurname().matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid surname. Only letters  allowed.\"}")
-                    .build();
+        if (!updatedAuthor.getSurname().matches("^[A-Za-z ]+$")) {
+            throw new InvalidInputException("Invalid surname. Only letters allowed.");
         }
 
         if (updatedAuthor.getAge() < 1 || updatedAuthor.getAge() > 100) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Invalid age. Age must be between 0 and 100.\"}").build();
+            throw new InvalidInputException("Invalid age. Age must be between 0 and 100.");
         }
-        if (AuthorDataStore.getAuthor(updatedAuthor.getName(),updatedAuthor.getSurname()) != null) {
-            return Response.status(Response.Status.CONFLICT)
-                    .entity("{\"error\":\"Author with this name is already registered.\"}")
-                    .build();
+
+        if (AuthorDataStore.getAuthor(updatedAuthor.getName(), updatedAuthor.getSurname()) != null) {
+            throw new InvalidInputException("Author with this name is already registered.");
         }
+
         Author existingAuthor = AuthorDataStore.getAuthor(name, surname);
         if (existingAuthor == null) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("{\"error\":\" Author not found.\"}")
-                    .build();
+            throw new AuthorNotFoundException("Author not found.");
         }
 
         AuthorDataStore.updateAuthor(name, surname, updatedAuthor);
@@ -168,8 +129,5 @@ public class AuthorResource {
                 .entity("{\"error\":\"Author updated successfully.\"}")
                 .build();
     }
-
-
-
 
 }
