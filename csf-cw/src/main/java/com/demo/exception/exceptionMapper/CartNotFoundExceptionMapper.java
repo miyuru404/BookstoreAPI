c6f62758c64e5141783bf522644f79637d7bfb28
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:33:09 +0530

    update:complete ExceptionMapper class

--- csf-cw/src/main/java/com/demo/exception/exceptionMapper/CartNotFoundExceptionMapper.java
+++ csf-cw/src/main/java/com/demo/exception/exceptionMapper/CartNotFoundExceptionMapper.java
@@ -1,4 +1,16 @@
 package com.demo.exception.exceptionMapper;
 
-public class CartNotFoundExceptionMapper {
+import com.demo.exception.CartNotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CartNotFoundExceptionMapper implements ExceptionMapper<CartNotFoundException> {
+    @Override
+    public Response toResponse(CartNotFoundException ex) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("{\"error\": \"" + ex.getMessage() + "\"}")
+                .build();
+    }
 }
