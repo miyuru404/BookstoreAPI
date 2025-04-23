64c4b4c8833ab1320a8574bec1c2163c1af1a48f
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-04-23 20:14:34 +0530

    feat:add CartNotFoundException  class

--- csf-cw/src/main/java/com/demo/exception/CartNotFoundException.java
+++ csf-cw/src/main/java/com/demo/exception/CartNotFoundException.java
@@ -1,4 +1,7 @@
 package com.demo.exception;
 
-public class CartNotFoundException {
+public class CartNotFoundException extends RuntimeException {
+    public CartNotFoundException(String message) {
+        super(message);
+    }
 }
